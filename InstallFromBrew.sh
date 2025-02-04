5d387453b4356fbba7e82a81a2806b28577a32b9
Tahsin Kabir <tahsinkabir@gmail.com>
2025-02-04 00:50:34 -0500

    updating install from brew

--- InstallFromBrew.sh
+++ InstallFromBrew.sh
@@ -3,11 +3,9 @@
 #Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install asciinema      #bash command flow recorder
-brew install asdf           #project specific version manager
 brew install awscli         #the aws cli
+brew install asciinema      #bash command flow recorder
 brew install cask           #cask is a non command prompt installer
-brew install ctags          #ze ctags for source code exploration
 brew install ctop           #the container top
 brew install fzf            #command line fuzzy finder
 brew install iftop          #network top
@@ -16,12 +14,21 @@ brew install kafkacat       #the kafka cat
 brew install maven          #for maven based java project dependency manager
 brew install openssl        #open ssl
 brew install ranger         #termincal gui based file browser
-brew install sbt            #sbt, the interactive build tool for scala and java 
-brew install scala          #scala, the func lang in JMV (for shobdo)
 brew install tmux           #the terminal multiplexer
 brew install tree           #tree view of files
 brew install watch          #watch the whole world burn
 brew install wget           #the terminal based downloader
+brew install htop           #the process top
+brew install ncdu           #the disk usage analyzer
+brew cask install docker    #docker, the container manager
+brew install jq             #jq, the json processor
+brew install git            #git, the version control system
+brew install terraform      #infrastructure as code tool
+brew install kubectl        #the kubernetes cli
+brew install poetry         #the python package manager
 
-brew install docker --cask    #docker, the container manager
-brew install java --cask      #the jdk
+
+# brew cask install java      #the jdk
+# brew install asdf           #project specific version manager
+# brew install sbt            #sbt, the interactive build tool for scala and java 
+# brew install scala          #scala, the func lang in JMV (for shobdo)
